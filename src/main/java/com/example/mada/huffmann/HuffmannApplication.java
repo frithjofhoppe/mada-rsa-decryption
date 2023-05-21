0d84dd7dc7b332b8a538bb2210b192530030036a
frithjofhoppe <frithjof.hoppe@gmail.com>
2023-05-21 12:19:12 +0200

    fix: Add alternativ path for mac os

--- src/main/java/com/example/mada/huffmann/HuffmannApplication.java
+++ src/main/java/com/example/mada/huffmann/HuffmannApplication.java
@@ -12,7 +12,10 @@ public class HuffmannApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(RSAApplication.class.getResource("/com/example/mada/huffmann.fxml"));
+        var os = System.getProperty("os.name");
+        System.out.println(os);
+        var filePath = os.contains("Windows") ? "/com/example/mada/huffmann.fxml" : "\\com\\example\\mada\\huffmann.fxml";
+        FXMLLoader fxmlLoader = new FXMLLoader(RSAApplication.class.getResource(filePath));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Huffmann Util");
         stage.setScene(scene);
